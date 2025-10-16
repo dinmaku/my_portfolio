5f26e948eba22e79397a6225642fccc59aa650b9
dinmaku <deanmark.belano@gmail.com>
2025-10-16 08:43:34 +0800

    Responsiveness Adjustments

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my_portfolio/',  // Add this line for GitHub Pages deployment
   plugins: [
     vue(),
     tailwind(),
